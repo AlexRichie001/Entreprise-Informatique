6901eedb87804f05cb6d48651f26347193dfd95c
Djialeu Poumo Alex Richie <djialeupoumoalexrichie@gmail.com>
2025-01-19 09:14:29 +0800

    Initial commit for ReactJS site

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue sur notre entreprise informatique pour recherche</h1>
+        <p>
+          Nous offrons des solutions informatiques personnalisées pour vos besoins professionnels.
+        </p>
+        <button>Contactez-nous</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
